fcbbad661d67dca0ba8023e80d21006fa2ae8bbb
Luiz <gustavoooliveira111@gmail.com>
2022-11-16 15:30:30 -0300

    add product scheme

---
+++ src/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    name: string;
+    price: number;
+    amount: number;
+
+    constructor(name: string, price: number, amount: number) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+}
